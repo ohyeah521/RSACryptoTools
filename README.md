fdd05bfe1d7de5043e2f083ab7b4a2cb80078d0f
wjbsyc <wjbsyc@hotmail.com>
2018-01-29 19:45:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# homuraCryptoTools
+
+因为被py的库函数坑了无数次,所以决定把自己常用的函数打个包,暂时就放了DES Wiener 和 MD5 以后有想到别的再加
+
+from homura import *
+wiener(e,n)
+md5s("string")
+
+s=des("imnotkey")
+s.setMode(EBC)
+str="string"
+s.encrypt(str)
+
+
+
+
+安装
+```
+git clone
+```
---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+  
+setup(name = 'homura',
+      version = '1.0',
+      py_modules = ['homura'],
+      packages = [""]#'Arithmetic','ContinuedFractions','MillerRabin','RSAvulnerableKeyGenerator','RSAwienerHacker'],
+     )
